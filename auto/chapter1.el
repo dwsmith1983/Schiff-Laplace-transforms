a7c67809d233e2e6eef017e6cb40a524ab9d51f5
Dustin <dustin.smith@uvm.edu>
2015-04-02 23:27:40 -0400

    update

--- auto/chapter1.el
+++ auto/chapter1.el
@@ -2,10 +2,13 @@
  "chapter1"
  (lambda ()
    (LaTeX-add-labels
-    "1c"
+    "ch1sec1prob1c"
     "ch1sec1prob2a"
     "ch1sec1prob2b"
     "ch1sec1"
     "ch1sec2prob1aLT"
-    "ch1sec2prob1a")))
+    "ch1sec2prob1a"
+    "ch1sec2prob1b"
+    "ch1sec2prob1c"
+    "ch1sec2prob3")))
 
